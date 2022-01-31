61f93b855cf55501de7d98dbcb6ec0927aca5822
Karen <karenmoncada80@gmail.com>
2022-01-31 10:08:35 -0300

    Cambios en el carrito

--- JS/carrito.js
+++ JS/carrito.js
@@ -1,31 +1,49 @@
 
+//Se crean las funciones para hacer funcionar el carrito
 
-
+//Guardar prendas en local storage
 function guardarPrendas2 (prendas){
 localStorage.setItem("prendas", JSON.stringify(prendas));
 console.log("Guardado");
 }
 
+//cargar prendas de la local storage
+//CON ESTO SE OBTIENEN LAS PRENDAS AÑADIDAS
 function cargarPrendas2(){
     console.log("cargado");
-    return JSON.parse(localStorage.getItem("prendas"));
+    let itemsForCart= JSON.parse(localStorage.getItem("clothesForCart"));
+   
+    actualizarContador()
+    return itemsForCart;
     
 }
 
+
+
+//ejecuta las funciones
 guardarPrendas2(prendas);
 cargarPrendas2();
 
+//Contador del carrito de compras (aparece debajo del dibujo del carrito)
+//CANTIDAD DE PRODUCTOS
 function actualizarContador (){
     let total = 0; 
+    let itemsInCart=JSON.parse(localStorage.getItem("clothesForCart"));
+  
+    if(itemsInCart && itemsInCart.length>0){
+        localStorage.setItem("totalCarrito", itemsInCart.length);
+    }
+
     if (localStorage.getItem("totalCarrito")) {
-    total = localStorage.getItem("totalCarrito");
+        total = localStorage.getItem("totalCarrito");
+    }else{
+        localStorage.setItem("totalCarrito", 0)
+        total = localStorage.getItem("totalCarrito");
     }
     
     let contador = document.getElementById("datosCarrito");
     contador.innerHTML = "<p>" + total+ " producto(s) </p>"
 }
-
-actualizarContador();
 
 function buscarPrendas (id){
     let prendas = cargarPrendas2();
@@ -41,23 +59,63 @@ return [];
 
 function agregarCarrrito(id){
     let prenda = buscarPrendas(id);
-    let prendas = cargarCarrito();
+    let prendas2 = cargarCarrito();
     prendas.push(prenda);
-    localStorage.setItem("cart", JSON.stringify(prendas));
-    localStorage.setItem("totalCarrito", prendas.length);
+    localStorage.setItem("cart", JSON.stringify(prendas2));
+    localStorage.setItem("totalCarrito", prendas2.length);
     actualizarContador();
    
 }
 
 
 function eliminarCarrito(){
-    localStorage.removeItem("cart");
     localStorage.removeItem("totalCarrito");
+    localStorage.removeItem("clothesForCart");
     console.log("Carrito vacio ");
     actualizarContador();
    
 }
- document.getElementById("eliminar").addEventListener("click", eliminarCarrito);
+ document.getElementById("vaciar-carrito").addEventListener("click", eliminarCarrito);
+
+ function AddTable(){
+   let subTotal = 0;
+   let descuento = 0;
+   let total = 0;
+   
+
+let cartBody = "";
+for( let prenda of itemsForCart){
+    cartBody += `
+    
+    <td> <img src="../Images/${prenda.imagen}" width='100'</td>
+    <td> ${prenda.nombre}</td>
+    <td>${prenda.precio}</td>
+    <td> ${prenda.quantity}</td>
+    <td> <a href='#' class='delete fas fa-times-circle' data-id=${prenda.id} id='deleteBtn'</td>
+    `
+    subTotal += prenda.precio;
+    descuento += prenda.precio * 0.85;
+    total += parseFloat(prenda.precio - descuento);
+    const idPrenda = prenda.id;
+    
+    document.getElementById("deleteBtn").onclick = async () => {
+    localStorage.removeItem(idPrenda);
+    localStorage.removeItem("clothesForCart");
+    console.log("Se elimino el producto "+idPrenda+"");
+    actualizarContador();}
+    } 
+    cartBody += `<td>${subTotal}</td>;
+    <td> ${descuento}</td>;
+    <td> ${total} </td>;`
+
+
+    }
+
+
+document.getElementById("lista-compra").innerHTML = cartBody;
+actualizarContador();
+
+ 
 
 
 /* let button = document.getElementById("boton-agregar");
@@ -68,39 +126,25 @@ actualizarContador();
 console.log("Se agrego "+prendas.nombre+"al carrito");} */
 
 
-agregarCarrrito(prendas.id);
-console.log("Se agrego al carrito");
+/* agregarCarrrito(prendas.id);
+console.log("Se agrego al carrito"); */
+/* 
+class Carrito {
+    comprarProducto(e){
+        e.preventDefault();
+        const producto = e.target.parentElement.parentElement;
+        //this.leerDatosProducto(producto);
+        console.log(producto);
 
-let productosCarrito = JSON.parse(localStorage.getItem("cart"));
-let total = 0;
-let body = "<table class='table'>";
-body +="<thead>";
-body +="<tr>";  
-body +=" <th scope='col'>Cantidad</th>";   
-body +="<th scope='col'>Penda</th>";    
-body +="<th scope='col'>Descripcion</th>";    
-body += "<th scope='col'>Precio</th>";  
-body +="<th scope='col'>Cantidad</th>";  
-body +="</tr>";  
-body +="</thead>";
-body +="<tbody>";
-  for(let prenda of productosCarrito){
-    body += "<tr>"
-    body +="<td><img src='//Images/"+prenda.imagen+"' alt='"+prenda.nombre+"' width=80px</td>";
-    body +="<td>"+prenda.nombre+"<td>";
-    body +="<td>"+prenda.precio+"<td>";
-    body +="</tr>";
- 
-      total += prenda.precio;
-  }
-  body += "<tr>";
-  body +="<td>&nbsp<ts>";
-  body +="<td>Total<td>";
-  body += "<td>$"+total+"<td>";
-  body +="</tr>";
-  body +="</tbody>";
-  body +="</table>";
-  
-  document.getElementById("carritoCompras").innerHTML = body; 
+    }
+}
+const carro = new Carrito();
+const carrito = document.getElementById("carrito");
+const productos = document.getElementById("prendas") ;
+const listaProductos = document.querySelector("#lista-carrito tbody");
 
-  document.getElementById("carritoCompras").addEventListener("click", agregarCarrrito);
+cargarEventos();
+
+function cargarEventos(){
+productos.addEventListener("click", (e) => carro.comprarProducto())
+} */
--- JS/index.js
+++ JS/index.js
@@ -1,33 +1,40 @@
 //Lista de productos 
 const prendas = [
-{id:1, nombre:"Campera White", imagen: "ary-milligan-nLVyej-i2N4-unsplash.jpg", precio:"5800"},
-{id:2, nombre: "Remera Black Slim Fit", imagen: "ian-dooley-1yl3jzKoKXg-unsplash.jpg", precio:"2300" },
-{id:3, nombre: "Jean Slim Fit", imagen: "vladimir-fedotov-MPfyh3xJ1iE-unsplash.jpg", precio:"4800" },
-{id:4, nombre: "Jean Holgado Claro ", imagen: "hans-isaacson-QJfXnkKrtlo-unsplash.jpg", precio:"5300" },
-{id:5, nombre: "Jean Roto con Parches", imagen: "alicia-petresc-BciCcl8tjVU-unsplash.jpg", precio:"4300" },
-{id:6, nombre: "Blazer Black", imagen: "joshua-rondeau-3_sGB2ON4X0-unsplash.jpg", precio:"4600" },
+{id:1, nombre:"Campera White", imagen: "ary-milligan-nLVyej-i2N4-unsplash.jpg", precio:"5800", quantity: 0},
+{id:2, nombre: "Remera Black Slim Fit", imagen: "ian-dooley-1yl3jzKoKXg-unsplash.jpg", precio:"2300", quantity:0 },
+{id:3, nombre: "Jean Slim Fit", imagen: "vladimir-fedotov-MPfyh3xJ1iE-unsplash.jpg", precio:"4800", quantity:0 },
+{id:4, nombre: "Jean Holgado Claro ", imagen: "hans-isaacson-QJfXnkKrtlo-unsplash.jpg", precio:"5300", quantity:0 },
+{id:5, nombre: "Jean Roto con Parches", imagen: "alicia-petresc-BciCcl8tjVU-unsplash.jpg", precio:"4300", quantity:0 },
+{id:6, nombre: "Blazer Black", imagen: "joshua-rondeau-3_sGB2ON4X0-unsplash.jpg", precio:"4600", quantity:0 },
 ]
-//Defino las variables 
-class ropa {
-    constructor (denim){
-        this.nombre = denim.nombre;
-        this.precio = denim.precio;
-        this.imagen = denim.imagen;
-        this.descuento = 0.85;
-        this.iva = 1.21;}
-        //calculo del precio con descuento 
-        aplicarDescuento(){ 
-            return (this.precio * this.descuento)};
-        
-           
-        
-    }
 
 //Se guardan los productos en la local storage
 function guardarPrendas (prendas){
     localStorage.setItem("prendas", JSON.stringify(prendas));
     console.log("las prendas fueron guardadas correctamente");
 }
+
+//Se guardan los productos para el carrito
+
+//AÑADIR ELEMENTOS AL CARRITO
+function addElsForCart(prenda){
+    // Parse any JSON previously stored in allEntries
+ 
+    let clothesArr = JSON.parse(localStorage.getItem("clothesForCart"));
+    if(clothesArr == null) clothesArr = [];
+    if(clothesArr.some(x=> x.id===prenda.id)){
+        let index=clothesArr.findIndex(x=>x.id===prenda.id);
+        clothesArr[index].quantity+=1; 
+       
+    }else{
+        prenda.quantity=1;
+        clothesArr.push(prenda);
+    }
+    // Save allEntries back to local storage
+    localStorage.setItem("clothesForCart", JSON.stringify(clothesArr));
+    cargarPrendas2();
+}
+
 
 //Se cargan los productos de la local storage
 function cargarPrendas (){
@@ -42,28 +49,44 @@ let contenido = "";
 
 
 //Se genera el codigo HTML para cargar las cards con la info de los productos 
-for (let prendaCargada of prendasCargadas) {
+for (let prenda of prendas) {
 
-    let Ropa = new ropa(prendaCargada);
-   
+  
     contenido += " <div class=' card p-2 g-4' style='width: 15rem;'>";
     contenido += "<h3 class='card-tittle fs-3 text-danger'> -15%</h3>";
-    contenido += "<img src='/Images/"+Ropa.imagen +"' class='card-img-top' alt='"+Ropa.nombre +"'>";
+    contenido += "<img src='../Images/"+prenda.imagen +"' class='card-img-top' alt='"+prenda.nombre +"'>";
     contenido += "<div class='card-body'>";
-    contenido += "<h5 class='card-title '>"+Ropa.nombre+"</h5>";
+    contenido += "<h5 class='card-title '>"+prenda.nombre+"</h5>";
     contenido += "<div class='row mb-2'>";
-    contenido += "<div class=' card-text text-muted text-decoration-line-through'><span>$" +Ropa.precio+"</span></div>";
-    contenido += "<div class=' card-text'><span>"+Ropa.aplicarDescuento()+"</span></div>";
+    contenido += "<div class=' card-text text-muted text-decoration-line-through'><span>$" +prenda.precio+"</span></div>";
+    contenido += "<div class=' card-text'><span>"+prenda.precio*0.85+"</span></div>";
     contenido += "<div class=' card-text bg-dark text-light'><span>6 cuotas sin interes</span></div>";
     contenido += "</div>";
     contenido += "<div>";
-    contenido += "<button type='button' class='btn btn-dark  btn-md ' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasRight' aria-controls='offcanvasRight'Toggle right offcanvas id='boton-agregar'>Agregar</button>";
+    //EVENTO PARA EL BOTON, AL HACER CLICK SE EJECUTA ADD ELEMENT
+    contenido += "<button type='button' class='btn btn-dark  btn-md ' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasRight' aria-controls='offcanvasRight'Toggle right offcanvas id='boton-agregar"+prenda.id+"' onclick=addElement("+prenda.id+")>Agregar</button>";
     contenido += "<div class='offcanvas offcanvas-end' tabindex='-1' id='offcanvasRight' aria-labelledby='offcanvasRightLabel'>";
     contenido += "<div class='offcanvas-header'>";
     contenido += "<h5 id='offcanvasRightLabel'>Carrito</h5>";
     contenido += "<button type='button' class='btn-close text-reset' data-bs-dismiss='offcanvas' aria-label='Close'></button>";
     contenido += " </div>"
     contenido += "<div class='offcanvas-body'>";
+    contenido += `
+                <table id="lista-carrito" class="tableOffCanvas">
+                <thead>
+                <tr>
+                    <th>Imagen </th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th></th>
+                </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+
+                <a href="#" id="vaciar-carrito" class="btn btn-primary btn-block">Vaciar Carrito</a>
+                <a href="#" id="procesar-pedido" class="btn btn-danger btn-block">Procesar
+                    Compra</a>`
     contenido += "</div>";
     contenido += "</div>";
     contenido += "</div>";
@@ -72,9 +95,17 @@ for (let prendaCargada of prendasCargadas) {
  
 }
 
-
 //Se cargan las cards con la info de los productos al HTML
 let prendasOferta = document.getElementById("prendas");
 prendasOferta.innerHTML = contenido;
 
+
+function addElement(prendaId){
+    //let elToAdd=prendas.find(x => x.id === prenda);
+    let elToAdd= prendas.find(function(x){
+       return x.id===prendaId;
+    })
+  
+    addElsForCart(elToAdd);
+}
 
